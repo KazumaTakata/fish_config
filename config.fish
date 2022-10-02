826c7459a5e4ad9dba802bcabc0d01963bdc0a0a
Kazuma Takata <neilbuzztakata@gmail.com>
2022-10-02 17:14:24 +0900

    init

---
+++ config.fish
@@ -0,0 +1,10 @@
+fish_vi_key_bindings
+
+
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+# Generated for envman. Do not edit.
+test -s "$HOME/.config/envman/load.fish"; and source "$HOME/.config/envman/load.fish"
+
